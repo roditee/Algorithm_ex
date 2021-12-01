bd82a2b1952f8e162d02830b9a9478fc58a34cc0
roditee <roditee@naver.com>
2021-12-01 18:08:53 +0900

    211201 정렬, 탐색, 트리

---
+++ src/tree/Node.java
@@ -0,0 +1,13 @@
+package tree;
+
+public class Node {
+	int value;
+	Node leftChild;
+	Node rightChild;
+	
+	public Node(int value) {
+		this.value = value;
+		leftChild = null;
+		rightChild = null;
+	}
+}
